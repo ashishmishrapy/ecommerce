747c757f11e530a8ab92134c1923fa7e10113838
Ashish-web-tech <akonwtf8@gmail.com>
2025-08-02 23:15:19 +0530

    updated

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -1,102 +1,157 @@
-// import React from 'react'
-// import s1 from '../../../assets/corosolImg/s1.jpeg'
-// import s2 from '../../../assets/corosolImg/s2.jpeg'
-// import s3 from '../../../assets/corosolImg/s3.jpeg'
-// import s4 from '../../../assets/corosolImg/s4.jpeg'
-// import CircleCategory from './CircleCategory'
+// // import React from 'react'
+// // import s1 from '../../../assets/corosolImg/s1.jpeg'
+// // import s2 from '../../../assets/corosolImg/s2.jpeg'
+// // import s3 from '../../../assets/corosolImg/s3.jpeg'
+// // import s4 from '../../../assets/corosolImg/s4.jpeg'
+import CircleCategory from './CircleCategory'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 
 
 // const Hero = () => {
-//   return (
-//     <div>
-//       <div className="carousel w-full h-[250px]  md:h-[600px] lg:h-[450px]">
+// //   return (
+// //     <div>
+// //       <div className="carousel w-full h-[250px]  md:h-[600px] lg:h-[450px]">
 
-//         <div id="slide1" className="carousel-item relative w-full snap-center shrink-0">
-//           <img src={s1} className="w-full" />
-//           <div className="hidden md:flex absolute left-5 right-5 top-1/2 -translate-y-1/2 transform justify-between">
-//             <a href="#slide4" className="btn btn-circle">❮</a>
-//             <a href="#slide2" className="btn btn-circle">❯</a>
-//           </div>
-//         </div>
+// //         <div id="slide1" className="carousel-item relative w-full snap-center shrink-0">
+// //           <img src={s1} className="w-full" />
+// //           <div className="hidden md:flex absolute left-5 right-5 top-1/2 -translate-y-1/2 transform justify-between">
+// //             <a href="#slide4" className="btn btn-circle">❮</a>
+// //             <a href="#slide2" className="btn btn-circle">❯</a>
+// //           </div>
+// //         </div>
 
-//         <div id="slide2" className="carousel-item relative w-full snap-center shrink-0">
-//           <img src={s2} className="w-full" />
-//           <div className="hidden md:flex absolute left-5 right-5 top-1/2 -translate-y-1/2 transform justify-between">
-//             <a href="#slide1" className="btn btn-circle">❮</a>
-//             <a href="#slide3" className="btn btn-circle">❯</a>
-//           </div>
-//         </div>
+// //         <div id="slide2" className="carousel-item relative w-full snap-center shrink-0">
+// //           <img src={s2} className="w-full" />
+// //           <div className="hidden md:flex absolute left-5 right-5 top-1/2 -translate-y-1/2 transform justify-between">
+// //             <a href="#slide1" className="btn btn-circle">❮</a>
+// //             <a href="#slide3" className="btn btn-circle">❯</a>
+// //           </div>
+// //         </div>
 
-//         <div id="slide3" className="carousel-item relative w-full snap-center shrink-0">
-//           <img src={s3} className="w-full" />
-//           <div className="hidden md:flex absolute left-5 right-5 top-1/2 -translate-y-1/2 transform justify-between">
-//             <a href="#slide2" className="btn btn-circle">❮</a>
-//             <a href="#slide4" className="btn btn-circle">❯</a>
-//           </div>
-//         </div>
+// //         <div id="slide3" className="carousel-item relative w-full snap-center shrink-0">
+// //           <img src={s3} className="w-full" />
+// //           <div className="hidden md:flex absolute left-5 right-5 top-1/2 -translate-y-1/2 transform justify-between">
+// //             <a href="#slide2" className="btn btn-circle">❮</a>
+// //             <a href="#slide4" className="btn btn-circle">❯</a>
+// //           </div>
+// //         </div>
 
-//         <div id="slide4" className="carousel-item relative w-full snap-center shrink-0">
-//           <img src={s4} className="w-full" />
-//           <div className="hidden md:flex absolute left-5 right-5 top-1/2 -translate-y-1/2 transform justify-between">
-//             <a href="#slide3" className="btn btn-circle">❮</a>
-//             <a href="#slide1" className="btn btn-circle">❯</a>
-//           </div>
-//         </div>
+// //         <div id="slide4" className="carousel-item relative w-full snap-center shrink-0">
+// //           <img src={s4} className="w-full" />
+// //           <div className="hidden md:flex absolute left-5 right-5 top-1/2 -translate-y-1/2 transform justify-between">
+// //             <a href="#slide3" className="btn btn-circle">❮</a>
+// //             <a href="#slide1" className="btn btn-circle">❯</a>
+// //           </div>
+// //         </div>
 
-//       </div>
+// //       </div>
 
-//       <CircleCategory/>
+// //       <CircleCategory/>
 
-//     </div>
-//   )
-// }
+// //     </div>
+// //   )
+// // }
 
-// export default Hero
+// // export default Hero
 
 
 
-import React, { useEffect, useState } from 'react';
-import s2 from '../../../assets/corosolImg/7.webp';
-import s3 from '../../../assets/corosolImg/6.webp';
-import s1 from '../../../assets/corosolImg/5.webp';
-import s4 from '../../../assets/corosolImg/4.webp';
-import CircleCategory from './CircleCategory';
+// import React, { useEffect, useState } from 'react';
+// import s2 from '../../../assets/corosolImg/7.webp';
+// import s3 from '../../../assets/corosolImg/6.webp';
+// import s1 from '../../../assets/corosolImg/5.webp';
+// import s4 from '../../../assets/corosolImg/4.webp';
+// import CircleCategory from './CircleCategory';
 import WomenCategoryGrid from './WomenCategoryGrid';
 import MenCategoryGrid from './MenCategoryGrid';
 import WomenCollectionGrid from './WomenCollectionGrid';
 import MenCollectionGrid from './MenCollectionGrid';
 
-const slides = [s1, s2, s3, s4];
+// const slides = [s1, s2, s3, s4];
 
 const Hero = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
+//   const [currentSlide, setCurrentSlide] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentSlide((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
-    }, 1000); 
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCurrentSlide((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+//     }, 1000); 
 
-    return () => clearInterval(interval); 
-  }, []);
+//     return () => clearInterval(interval); 
+//   }, []);
 
-  return (
-    <div>
-      <div className="relative w-full h-[250px] md:h-[600px] lg:h-[450px] overflow-hidden">
-        <img
-          src={slides[currentSlide]}
-          alt={`Slide ${currentSlide + 1}`}
-          className="w-full h-full object-left-top transition-all duration-1000"
-        />
-      </div>
 
-      <CircleCategory />
-      <WomenCategoryGrid/>
-      <MenCategoryGrid/>
-      <WomenCollectionGrid/>
-      <MenCollectionGrid/>
-    </div>
-  );
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 1
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
 };
 
+
+  return (
+//     <div>
+//       <div className="relative w-full h-[250px] md:h-[600px] lg:h-[450px] overflow-hidden">
+//         <img
+//           src={slides[currentSlide]}
+//           alt={`Slide ${currentSlide + 1}`}
+//           className="w-full h-full object-left-top transition-all duration-1000"
+//         />
+//       </div>
+
+//       <CircleCategory />
+//       <WomenCategoryGrid/>
+//       <MenCategoryGrid/>
+//       <WomenCollectionGrid/>
+//       <MenCollectionGrid/>
+//     </div>
+//   );
+// };
+
+<>
+
+
+<Carousel autoPlay autoPlaySpeed={1500} showDots swipeable infinite removeArrowOnDeviceType={["tablet", "mobile","desktop", "superLargeDesktop"]} responsive={responsive}>
+  <div>
+    <img className="max-h-[70vh] min-w-full" src="../../../assets/corosolImg/4.webp" alt="" />
+  </div>
+  <div>
+    <img className="max-h-[70vh] min-w-full"  src="../../../assets/corosolImg/5.webp" alt="" />
+  </div>
+  <div>
+    <img className="max-h-[70vh] min-w-full" src="../../../assets/corosolImg/6.webp" alt="" />
+  </div>
+  <div>
+    <img className="max-h-[70vh] min-w-full" src="../../../assets/corosolImg/7.webp" alt="" />
+  </div>
+</Carousel>
+
+<CircleCategory />
+    <WomenCategoryGrid/>
+       <MenCategoryGrid/>
+       <WomenCollectionGrid/>
+       <MenCollectionGrid/>
+
+
+</>
+
+ );
+};
+ 
+ 
 export default Hero;
